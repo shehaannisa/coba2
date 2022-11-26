8bd4779edf4b881e6691f0deb46bfd267843ec52
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-26 17:34:27 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
 // const mongoDB = 'mongodb://localhost:27017/Project-2-BE12';
-const mongoDB = 'mongodb://mongo:3qTUFD0S6S1BRmaUK6Ss@containers-us-west-115.railway.app:6298';
+const mongoDB = 'mysql://root:NTIFTnAXj3bXBQOh7iEk@containers-us-west-65.railway.app:7397/railway';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
