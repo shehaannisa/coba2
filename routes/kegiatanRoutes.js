c7633de2781b65d7a9799e60a0cc58e142e72ba9
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-29 21:50:25 +0700

    Add files via upload

---
+++ routes/kegiatanRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    uploadKegiatan,
+    getAllKegiatan,
+    getKegiatanByID,
+    deleteKegiatanByID,
+    updateKegiatanByID
+} = require("../controllers/kegiatanController");
+
+router.post("/postKegiatan", uploadKegiatan)
+router.get("/AllKegiatan", getAllKegiatan);
+router.get("/:id", getKegiatanByID);
+router.delete("/:id", deleteKegiatanByID);
+router.patch("/:id", updateKegiatanByID);
+
+module.exports = router;
