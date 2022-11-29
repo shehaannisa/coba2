74817f7606cd360a45daa169d28f573563eae68e
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-29 21:51:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('./config/database');
+// const authRoutes = require('./routes/authRoutes');
+// const userRoutes = require('./routes/userRoutes');
+// const cors = require('cors');
+
+// const app = express();
+
+// //connection to database
+// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// // middleware
+// app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(express.json());
+
+// //import
+// const adminRoutes = require('./routes/adminRoutes')
+
+// //ex
+// app.use('/admin', adminRoutes)
+
+// // routes
+// app.use(authRoutes);
+// app.use(userRoutes);
+
+
+// app.listen(process.env.PORT||5173, function () {
+//     console.log(`listen on port ${process.env.PORT || 5173}`);
+// });
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('./config/database');
@@ -17,17 +50,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
 
 //import
-const adminRoutes = require('./routes/adminRoutes')
-
-//ex
-app.use('/admin', adminRoutes)
+const kegiatanRoutes = require('./routes/kegiatanRoutes')
 
 // routes
 app.use(authRoutes);
 app.use(userRoutes);
+app.use(kegiatanRoutes);
 
 
 app.listen(process.env.PORT||5173, function () {
     console.log(`listen on port ${process.env.PORT || 5173}`);
 });
+
+
+
+
 
