6e6d70f6c393ac82b2b3843ed2a70676ef1c87fc
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 20:54:43 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,70 +1,107 @@
-const bcrypt = require('bcrypt');
-const User = require("../models/User");
+const User = require("../models/user");
+const ErrorResponse = require('../utils/errorResponse');
 
-module.exports = {
-  getAllUser: async(req, res) => {
-    try {
-      const users = await User.find({}, "-__v")
+
+exports.signup = async (req, res, next)=>{
+
+    const {email} = req.body;
+    const userExist = await User.findOne({email});
+    
+    // if (userExist){
       
-      res.status(200).json({
-        message: "Getting Data",
-        data: users
-      })
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" })
-    }
-  },
+    //  return  next(new ErrorResponse('E-mail already exists', 400))
+    // }
 
-  getUserByID: async(req, res) => {    
     try {
-      const users = await User.findById(req.params.id, "-__v")
-
-      if(!users){
-        res.status(404).json({
-          message : "Could not Found"
-        });
-    } else{
-      res.status(200).json({
-        message: "You Searched for",
-        data: users
-      })
-    }
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            user
+        })
+        
     } catch (error) {
-      res.status(500).json({ message: "Server Error" })
+        console.log(error);
+        next(error);
+        
     }
-  },
-
-  deleteUserByID: async (req, res) => {
-    try {
-      const users = await User.findById(req.params.id, "-__v")
-
-      if(!users){
-        res.status(404).json({
-          message : "Could not Found"
-        });
-    } else{
-      users.deleteOne()
-      res.status(201).json(
-        {message: "Data Deleted"
-      })
-    }
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" })
-    }
-  },
-
-  updateUserByID: async (req, res) => {
-    try {
-      const users = await User.findById(req.params.id, "-__v")
-
-      Object.assign(users, req.body)
-      users.save();
-      res.status(201).send({ 
-        message : "User updated!",
-        data : users })
    
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" })
+}
+
+
+exports.signin = async (req, res, next)=>{
+
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+       
+            return  next(new ErrorResponse('E-mail and password are required', 400))
+        }
+
+        // check user e-mail
+        const user = await User.findOne({email});
+        if(!user){
+           
+            return  next(new ErrorResponse('Invalid credentials', 400))
+        }
+
+        // verify user password
+        const isMatched = await user.comparePassword(password);
+        if (!isMatched){
+         
+          return  next(new ErrorResponse('Invalid credentials', 400))
+        }
+
+        generateToken(user, 200, res);
     }
-  }
+    catch(error){
+        console.log(error);
+       
+        next(new ErrorResponse('Cannot log in, check your credentials', 400))
+    }
+   
+}
+
+
+const generateToken = async (user, statusCode, res) =>{
+
+    const token = await user.jwtGenerateToken();
+
+    const options = {
+        httpOnly: true,
+        expires: new Date(Date.now() + process.env.EXPIRE_TOKEN)
+    };
+
+    res
+    .status(statusCode)
+    .cookie('token', token, options )
+    .json({success: true, token})
+}
+
+
+//LOG OUT USER
+exports.logout = (req, res, next)=>{
+    res.clearCookie('token');
+    res.status(200).json({
+        success: true,
+        message: "Logged out"
+    })
+}
+
+
+
+
+exports.singleUser = async (req, res, next)=>{
+
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json({
+            sucess: true,
+            user
+        })
+        
+    } catch (error) {
+        next(error)
+        
+    }
+   
 }
