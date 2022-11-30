16cb1c93488221d932c1acbabcea041f057278d6
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 08:55:35 +0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ const {
 
 router.get("/getAllUser", getAllUser);
 router.get("/:id", getUserByID);
-router.delete("/deluser/:id", deleteUserByID);
+router.delete("/:id", deleteUserByID);
 router.patch("/:id", updateUserByID);
 
 module.exports = router;
