1dc0a09f7e796ee15b6100b45d21c1bffda59f0c
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 21:10:55 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,130 +1,81 @@
-const User = require("../models/user");
-const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
+//handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { full_name: '', username: '', email: '', password: '', status: '' };
 
-exports.signup = async (req, res, next)=>{
-
-    const {email} = req.body;
-    const userExist = await User.findOne({email});
-    
-    if (userExist){
-      
-     return  next(new ErrorResponse('E-mail already exists', 400))
+    //validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
+        });
     }
 
-    try {
-        const user = await User.create(req.body);
-        res.status(201).json({
-            success: true,
-            user
-        })
-        
-    } catch (error) {
-        console.log(error);
-        next(error);
-        
+    // incorrect username
+    if (err.message === 'incorrect username') {
+        errors.username = 'that username is not registered';
     }
-   
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'that password is incorrect';
+    }
+
+    //duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        errors.username = 'that username is already registered';
+        return errors;
+    }
+
+    return errors;
 }
 
-
-exports.signin = async (req, res, next)=>{
-
-    try{
-        const {email, password} = req.body;
-        if(!email || !password){
-       
-            return  next(new ErrorResponse('E-mail and password are required', 400))
-        }
-
-        // check user e-mail
-        const user = await User.findOne({email});
-        if(!user){
-           
-            return  next(new ErrorResponse('Invalid credentials', 400))
-        }
-
-        // verify user password
-        const isMatched = await user.comparePassword(password);
-        if (!isMatched){
-         
-          return  next(new ErrorResponse('Invalid credentials', 400))
-        }
-
-        generateToken(user, 200, res);
-    }
-    catch(error){
-        console.log(error);
-       
-        next(new ErrorResponse('Cannot log in, check your credentials', 400))
-    }
-   
-}
-
-
-const generateToken = async (user, statusCode, res) =>{
-
-    const token = await user.jwtGenerateToken();
-
-    const options = {
-        httpOnly: true,
-        expires: new Date(Date.now() + process.env.EXPIRE_TOKEN)
-    };
-
-    res
-    .status(statusCode)
-    // .cookie('token', token, options )
-    .json({success: true, token})
-}
-
-
-//LOG OUT USER
-exports.logout = (req, res, next)=>{
-    res.clearCookie('token');
-    res.status(200).json({
-        success: true,
-        message: "Logged out"
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'BE 12', {
+        expiresIn: maxAge
     })
 }
 
+module.exports.signup_post = async (req, res) => {
+    const { full_name, username, email, password, status, } = req.body;
 
+    try {
+        const user = await User.create({ full_name, username, email, password, status });
+        res.status(201).json({ 
+            user: user._id,
+            username: user.username,
+            email: user.email,
+            status: user.status
+         });
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors })
+    }
+}
+module.exports.signin_post = async (req, res) => {
+    const { username, password } = req.body;
 
-// USESR PROFILE 
-exports.userProfile = async (req, res, next)=>{
-
-    const user = await User.findById(req.user.id);
-    res.status(200).json({
-        sucess: true,
-        user
-    });
+    try {
+        const user = await User.signin(username, password);
+        const token = createToken(user._id);
+        res.status(200).json({ 
+            user_id: user._id,
+            status: user.status,
+            user_token: token
+         })
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
 
-
-exports.getAllUser = async(req, res) => {
-    try {
-      const users = await User.find({}, "-__v")
-      
-      res.status(200).json({
-        message: "Getting Data",
-        data: users
-      })
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" })
-    }
-  };
-
-exports.singleUser = async (req, res, next)=>{
-
-    try {
-        const user = await User.findById(req.params.id);
-        res.status(200).json({
-            sucess: true,
-            user
-        })
-        
-    } catch (error) {
-        next(error)
-        
-    }
-   
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.send('user sudah keluar');
 }
