1f1c695b72fc98639214338cccd3722fd3338fdf
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 09:05:14 +0700

    Update kegiatanController.js

--- controllers/kegiatanController.js
+++ controllers/kegiatanController.js
@@ -2,24 +2,6 @@ const bcrypt = require('bcrypt');
 const Kegiatan = require("../models/Kegiatan");
 
 module.exports = {
-    uploadKegiatan: async (req, res) => {
-        const { judul_kegiatan, tgl_kegiatan, lokasi_kegiatan, deskripsi } = req.body;
-    
-        try {
-            const kegiatan = await Kegiatan.create({ judul_kegiatan, tgl_kegiatan, lokasi_kegiatan, deskripsi });
-            res.status(201).json({ 
-                kegiatan: kegiatan._id,
-                judul_kegiatan: kegiatan.judul_kegiatan,
-                tgl_kegiatan: kegiatan.tgl_kegiatan,
-                lokasi_kegiatan: kegiatan.lokasi_kegiatan,
-                deskripsi: kegiatan.deskripsi
-             });
-        }
-        catch (err) {
-            const errors = handleErrors(err);
-            res.status(400).json({ errors })
-        }
-    }, 
     getAllKegiatan: async(req, res) => {
         try {
             const kegiatans = await Kegiatan.find({}, "-__v")
@@ -51,6 +33,25 @@ module.exports = {
             res.status(500).json({ message: "Server Error" })
         }
     },
+    uploadKegiatan: async (req, res) => {
+        const { judul_kegiatan, tgl_kegiatan, lokasi_kegiatan, deskripsi } = req.body;
+    
+        try {
+            const kegiatan = await Kegiatan.create({ judul_kegiatan, tgl_kegiatan, lokasi_kegiatan, deskripsi });
+            res.status(201).json({ 
+                kegiatan: kegiatan._id,
+                judul_kegiatan: kegiatan.judul_kegiatan,
+                tgl_kegiatan: kegiatan.tgl_kegiatan,
+                lokasi_kegiatan: kegiatan.lokasi_kegiatan,
+                deskripsi: kegiatan.deskripsi
+             });
+        }
+        catch (err) {
+            const errors = handleErrors(err);
+            res.status(400).json({ errors })
+        }
+    }, 
+    
     
     deleteKegiatanByID: async (req, res) => {
         try {
